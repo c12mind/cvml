c9f1b584c5854ac9355fea966574d605c798d4e0
jayathungek <jayathunge.work@gmail.com>
2026-01-21 21:11:46 +0000

    generate result plots

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import json
 
 import torch
+import numpy as np
+from scipy.stats import t
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.size'] = 15
 
 
 def hysteresis_area(E, I, d):
@@ -38,3 +44,39 @@ def get_checkpoint(model_path, epoch):
             return checkpoint, config
     print(f"Epoch {epoch} not found in {model_path}")
     return None, config
+
+    
+def get_student_t(confidence_required, num_samples):
+    alpha = 1 - confidence_required
+    utp = 1 - (alpha/2)
+    dof = num_samples - 1
+    return t.ppf(utp, df=dof)
+
+    
+def plot_result(result_obj, offset_param=0.2):
+    experiments = result_obj["experiments"]
+    xs = 2 * np.arange(len(experiments))
+    plt.figure(figsize=(15, 5))
+    setting_names = result_obj["settings"].keys()
+    n = len(setting_names)
+    width = offset_param /  n
+    for i, setting_name in enumerate(setting_names):
+        offset = (i - (n-1)/2) * width
+        setting = result_obj["settings"][setting_name]
+        ys = np.array(setting["normed_pred"])
+        ci = np.array(setting["ci"])
+        plt.errorbar(xs + offset, ys, yerr=ci, fmt='o', capsize=4, label=setting_name)
+    plt.plot(xs, np.ones_like(xs), color="black", label="Target", alpha=0.5, linestyle="--")
+    plt.legend(ncols=5)
+    plt.ylabel("Average Normalised $C_{sp}$")
+    plt.xlabel("Experiment")
+    plt.xticks(xs, experiments)
+    plt.savefig("test.png")
+
+
+
+if __name__ == "__main__":
+    with open("saved_models/results.json", "r") as fh:
+        results = json.load(fh)
+    
+    plot_result(results, offset_param=0.8)
